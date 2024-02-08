6e4d69e5a44f2eae729c1db4b2c60e47d01e9cf2
AlbertQmoa <lovesphysics@gmail.com>
2024-02-08 20:04:08 +0800

    test utils in main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-v1
+from utils import ComplexNumber
+
+a = ComplexNumber(3, 2)
+b = ComplexNumber(1, 7)
+
+print(f'complex number a: {a}')
+print(f'complex number b: {b}')
+print(f'a + b = {a + b}')
+print(f'a - b = {a - b}')
+print(f'a * b = {a * b}')
+print(f'a / b = {a / b}')
